b58f7be154eeb87dbc486b7c038d3f3e2bb21eb4
Aigerim <aigerim.nuralieva@iaau.edu.kg>
2024-11-11 17:54:19 +0600

    category mvc

---
+++ src/main/java/com/example/mfu/controllers/CategoryController.java
@@ -0,0 +1,49 @@
+package com.example.mfu.controllers;
+
+import com.example.mfu.entities.Category;
+import com.example.mfu.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<Category>> getAllCategories() {
+        List<Category> categories = categoryService.findAllCategories();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getCategoryById(@PathVariable Integer id) {
+        Optional<Category> category = categoryService.findCategoryById(id);
+        return category.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryService.saveCategory(category);
+        return ResponseEntity.ok(savedCategory);
+    }
+
+    @PutMapping("update/{id}")
+    public ResponseEntity<Category> updateCategory(@PathVariable Integer id, @RequestBody Category category) {
+        category.setId(id);
+        Category updatedCategory = categoryService.saveCategory(category);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteCategory(@PathVariable Integer id) {
+        categoryService.deleteCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+}
